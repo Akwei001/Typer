dd5dd7fc762cbc62a3e7ff3b6497d1fa776ead20
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-07 22:02:04 +0000

    minor changes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+function App() {
+ 
+  return (
+    <div className="text-4xl text-center text-blue-500">
+     Hello world! It works! 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 �
+    </div>
+  )
+}
+
+export default App
