4097713fdea85dc707d60c850162039c4ab317a4
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-09 20:59:04 +0000

    texr

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
  
   return (
     <div className="text-4xl text-center text-blue-500">
-     Hello world! It works! 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 �
+     Hello world! It works now! 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 🚀 �
     </div>
   )
 }
