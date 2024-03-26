e33cbc9265e988bf792665717213905203f1abb7
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-26 21:54:52 +0000

    first animation

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -6,16 +6,21 @@ const Results = ({errors, accuracyPercentage, total, className} : {
     total: number
     className?: string;
 }) => {
-    const initial = {opacity: 0}
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+  const duration = { duration: 0.5 };
+  
 
   return (
-    <ul className={`flex flex-col items-center text-yellow-500 space-y-3 ${className}}`}>
-       <li className="text-xl font-semibold">Results</li>
-       <li>Accuracy: {accuracyPercentage}</li>
-       <li>Errors: {errors}</li>
-       <li> Typed: {total}</li> 
-    
-    </ul>
+    <motion.ul> <ul className={`flex flex-col items-center text-yellow-500 space-y-3 ${className}}`}>
+    <li className="text-xl font-semibold">Results</li>
+    <li>Accuracy: {accuracyPercentage}</li>
+    <li>Errors: {errors}</li>
+    <li> Typed: {total}</li> 
+ 
+ </ul>
+ </motion.ul>
+   
   )
 }
 
