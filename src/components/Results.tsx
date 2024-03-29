c4516bdcccec54baeb286f8a08b916bdd692e822
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-03-29 12:13:08 +0000

    results animation working

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -12,14 +12,36 @@ const Results = ({errors, accuracyPercentage, total, className} : {
   
 
   return (
-    <motion.ul> <ul className={`flex flex-col items-center text-yellow-500 space-y-3 ${className}}`}>
-    <li className="text-xl font-semibold">Results</li>
-    <li>Accuracy: {accuracyPercentage}</li>
-    <li>Errors: {errors}</li>
-    <li> Typed: {total}</li> 
+     <motion.ul className={`flex flex-col items-center text-yellow-500 space-y-3 ${className}}`}>
+
+      
+    <motion.li
+      initial={initial}
+      animate={animate}
+      className="text-xl font-semibold"
+      transition={{...duration, delay: 0}}
+      >
+        Results
+        </motion.li>
+    <motion.li
+      initial={initial}
+      animate={animate}
+      
+      transition={{...duration, delay: 0.5}}> Accuracy: {accuracyPercentage}</motion.li>
+
+    <motion.li
+      initial={initial}
+      animate={animate}
+      className="text-red-500"
+      transition={{...duration, delay: 1}}>Errors: {errors}</motion.li>
+    <motion.li
+      initial={initial}
+      animate={animate}
+      className="text-red-500"
+      transition={{...duration, delay: 1.5}}> Typed: {total}</ motion.li> 
  
- </ul>
  </motion.ul>
+ 
    
   )
 }
