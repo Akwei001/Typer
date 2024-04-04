375fbb633ce87d1f51a3c44af362d7f4317ca5f4
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-04-04 22:15:27 +0100

    caret framer motion

--- src/components/Caret.tsx
+++ src/components/Caret.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+
 
 const Caret = () => {
   return (
-    <div>Caret</div>
+    <motion.div
+    aria-hidden={true}
+    className='inline-block bg-yellow-500 w-o.5 h-7'
+    initial={{opacity:1}}
+    
+    />
   )
 }
 
