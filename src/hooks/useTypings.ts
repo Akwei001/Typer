2be60723fec71646807e0b0099ffbb7f907c5183
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-04-09 18:45:22 +0100

    keydownhandler

--- src/hooks/useTypings.ts
+++ src/hooks/useTypings.ts
@@ -1,5 +1,23 @@
+import { useCallback, useEffect, useState } from "react";
+
 const useTypings = (enabled: boolean) => {
-    const[cursor, setCursor]
+    const[cursor, setCursor] = useState(0);
+    const[typred, setTyped] = useState<string>("");
+    const totalTyped = useRef(0);
+
+const keydownHandler = useCallback(() => {},[])
+
+// attach the keydown event listener to record keystrokes
+useEffect( () => {
+    window.addEventListener("keydown", keydownHandler);
+
+    // Remove event listener on cleanup
+    return ( ) => {
+        window.removeEventListener("keydown", keydownHandler);
+    }
+
+},[keydownHandler])
+
 
 
 };
