52009ead55eb6fddc7883be8cc632dba5e573489
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-04-10 21:59:31 +0100

    iskeybord code allowed

--- src/hooks/useTypings.ts
+++ src/hooks/useTypings.ts
@@ -1,11 +1,41 @@
-import { useCallback, useEffect, useState } from "react";
+import { KeyboardEvent, useCallback, useEffect, useState } from "react";
+
+const isKeyboardCodeAllowed = (code: string) => {
+
+  return (
+    code.startsWith("Key") ||
+    code.startsWith("Digit") ||
+    code === "Backspace" ||
+    code === "Space"
+  );
+};
 
 const useTypings = (enabled: boolean) => {
     const[cursor, setCursor] = useState(0);
     const[typred, setTyped] = useState<string>("");
     const totalTyped = useRef(0);
 
-const keydownHandler = useCallback(() => {},[])
+    const keydownHandler = useCallback(
+        ({ key, code }: KeyboardEvent) => {
+          if (!enabled || !isKeyboardCodeAllowed(code)) {
+            return;
+          }
+    
+          switch (key) {
+            case "Backspace":
+              setTyped((prev) => prev.slice(0, -1));
+              setCursor((cursor) => cursor - 1);
+              totalTyped.current -= 1;
+              break;
+            default:
+              setTyped((prev) => prev.concat(key));
+              setCursor((cursor) => cursor + 1);
+              totalTyped.current += 1;
+          }
+        },
+        [enabled]
+      );
+    
 
 // attach the keydown event listener to record keystrokes
 useEffect( () => {
@@ -23,3 +53,17 @@ useEffect( () => {
 };
 
 export default useTypings;
+
+function useRef(arg0: number) {
+    throw new Error("Function not implemented.");
+}
+
+
+function isKeyboardCodeallowed(code: string) {
+    throw new Error("Function not implemented.");
+}
+
+
+function keydownHandler(this: Window, ev: KeyboardEvent) {
+    throw new Error("Function not implemented.");
+}
